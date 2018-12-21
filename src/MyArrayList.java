71ff8130420b295d28d186b0b7a00416ab0af4c4
Rocket Codester <45156318+RocketCodester@users.noreply.github.com>
2018-12-20 20:09:43 -0600

    Initial Commit

---
+++ src/MyArrayList.java
@@ -0,0 +1,20 @@
+
+//---------------------------------------------------------------
+//         Project
+//         MyArrayList.java
+//         Prints the keys of the alphabet array.
+//---------------------------------------------------------------
+
+public class MyArrayList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String[] alphabet = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+        for (int i = 0; i < alphabet.length; i++) {
+            System.out.println(i);
+        }
+    }
+    
+}
